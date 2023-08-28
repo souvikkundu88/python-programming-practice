73e4961d2a0a6ab3c1780ad0d60dddec283d135a
Souvik Kundu <kivtech88@gmail.com>
2023-08-28 16:57:58 +0530

    Update loop_double.py

--- loop_double.py
+++ loop_double.py
@@ -6,3 +6,15 @@ while idx < len(nums):
     num = nums[idx]
     print(num * num)
     idx += 1
+
+
+'''
+float_list = [2.5, 16.42, 10.77, 8.3, 34.21]
+count_greater = 0
+
+for num in float_list:  # Iterator traverses to the last index of the list
+    if num > 10:
+        count_greater += 1
+
+print(count_greater)
+'''
