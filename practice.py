0b475a800578044309a7178d38cf7a02e6868f33
Souvik Kundu <kivtech88@gmail.com>
2025-02-14 19:22:00 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,17 @@
 # print("hello world")
 # print("souvik")
 
+a = "souvik"
+print("a =", a)
+b = "kundu"
+print("b =", b)
+temp = b
+print("temp =", temp)
+b = a
+print("b =", b)
+l = "london"
+a = l
+print("a =", a)
+print("b =", b)
+print("\n")
+print("a =", a, "temp =", temp, "b =", b, "temp =", temp, "b =", b, "temp = ", temp, "a =", a, "b =", b, "temp =", temp, "b =", b)
