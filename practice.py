72d28042f45ab1af970911d73af258781318dcd2
Souvik Kundu <kivtech88@gmail.com>
2025-02-16 12:32:15 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,17 +1,20 @@
+# date: February 14, 2025 
+# learning python and practicing
 # print("hello world")
 # print("souvik")
 
-a = "souvik"
-print("a =", a)
-b = "kundu"
-print("b =", b)
-temp = b
-print("temp =", temp)
-b = a
-print("b =", b)
-l = "london"
-a = l
-print("a =", a)
-print("b =", b)
-print("\n")
-print("a =", a, "temp =", temp, "b =", b, "temp =", temp, "b =", b, "temp = ", temp, "a =", a, "b =", b, "temp =", temp, "b =", b)
+# a = "souvik"
+# print("a =", a)
+# b = "kundu"
+# print("b =", b)
+# temp = b
+# print("temp =", temp)
+# b = a
+# print("b =", b)
+# l = "london"
+# a = l
+# print("a =", a)
+# print("b =", b)
+# print("\n")
+# print("a =", a, "temp =", temp, "b =", b, "temp =", temp, "b =", b, "temp = ", temp, "a =", a, "b =", b, "temp =", temp, "b =", b)
+
